50a189efd8ada67af18fd53dd0c170b489ca75c3
David Rimar <rimardavid@gmail.com>
2020-09-19 09:53:54 +0200

    package.json, server.js, README

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const runServer = () => {
+  // Get the current working directory that the local server is being run from.
+  const cwd = process.cwd();
+
+  // Initialise the application with Express
+  const app = express();
+
+  // Connect the server to the root directory
+  app.use(express.static(cwd));
+
+  // Set the default port to connect to.
+  const port = 3000;
+
+  // Run the server at the specified port.
+  app.listen(port, () => {
+    console.log(`Server is up and running at port http://localhost:${port}`);
+  });
+};
+
+runServer();
